a0f15e51e69be3d14b33961d094891b8bcdb2cce
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2020-03-10 16:29:32 +0100

    app.py: Simplify into functions

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import os
 from flask import Flask
 
 
+def load_config(app):
+    import config
+
+    app.config.from_object(config)
+
+
+def make_instance_path(app):
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
 def register_routes_entry(app):
     import routes
 
@@ -10,19 +23,11 @@ def register_routes_entry(app):
         entry.register(app)
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
+    load_config(app)
+    make_instance_path(app)
     register_routes_entry(app)
 
     return app
