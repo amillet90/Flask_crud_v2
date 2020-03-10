438e0dfb0e99ed7a48156ecebfe2b46592fca33b
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2020-03-10 16:52:11 +0100

    Use templates for hello blueprint

--- blueprints/hello.py
+++ blueprints/hello.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 
 bp = Blueprint('hello', __name__, url_prefix='/')
@@ -6,4 +6,5 @@ bp = Blueprint('hello', __name__, url_prefix='/')
 
 @bp.route('/')
 def hello():
-    return 'Hello, world!'
+    name = request.args.get('name', 'world')
+    return render_template('hello.html', name=name)
