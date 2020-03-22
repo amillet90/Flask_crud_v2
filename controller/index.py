5d8bd258444e70f75a312d82d982eae24407821a
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2020-03-22 17:23:13 +0100

    Add simple Author model with controller

---
+++ controller/index.py
@@ -0,0 +1,9 @@
+from flask import *
+
+
+c = Blueprint('index', __name__, url_prefix='/')
+
+
+@c.route('/')
+def index():
+    return render_template('index.html')
---
+++ model/author.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Author {self.id}>'
