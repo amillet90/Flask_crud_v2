c538f3f2e36474ac66132609ee62077261411702
Muhammad Kaisar Arkhan <hi@yukiisbo.red>
2020-04-05 23:57:25 +0200

    Go back to flask run
    flask.run() is buggy and has a lot of issues due to how instancing is
    mismanaged.

--- app.py
+++ app.py
@@ -2,30 +2,29 @@ import os
 from logging.config import dictConfig
 from importlib import import_module
 
+import click
 from flask import Flask, Blueprint
+from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy
 
 import config
 
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object(config)
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-def load_logging_config():
+def load_logging_config(app):
     dictConfig(app.config['LOGGER_CONFIGURATION'])
 
 
-def make_instance_path():
+def make_instance_path(app):
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
 
-def load_all_controllers():
+def load_all_controllers(app):
     from Controller import AuthorController, MainController
 
     controllers = [AuthorController.c, MainController.c]
@@ -33,9 +32,37 @@ def load_all_controllers():
         app.register_blueprint(c)
 
 
-if __name__ == '__main__':
-    load_logging_config()
-    make_instance_path()
-    load_all_controllers()
+@click.command('create-all',
+               short_help='Initialize database with tables')
+@with_appcontext
+def create_all_tables():
+    db.create_all()
 
-    app.run(debug=True)
+
+@click.command('drop-all',
+               short_help='Drop all tables in database')
+@with_appcontext
+def drop_all_tables():
+    db.drop_all()
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object(config)
+    load_logging_config(app)
+    make_instance_path(app)
+    db.init_app(app)
+
+    from Entity.Author import Author
+    app.cli.add_command(create_all_tables)
+    app.cli.add_command(drop_all_tables)
+
+
+    load_all_controllers(app)
+
+    return app
+
+
+if __name__ == '__main__':
+    print("Run this application with 'flask run'.")
